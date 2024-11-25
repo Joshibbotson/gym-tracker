b467e6c4e3a65890aca76e8562cb27fb8425fce8
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2024-11-25 18:05:07 +0000

    setup login

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export type LoginConfig = {
+  email: string;
+  password: string;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  // replace with an env variable for production build.
+  private readonly apiUrl: string = 'http://localhost:8888';
+  private readonly http = inject(HttpClient);
+
+  login(query: LoginConfig): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/auth/login`, query);
+  }
+}
--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import {
   FormGroup,
   ReactiveFormsModule,
   Validators,
   FormControl,
 } from '@angular/forms';
+import { AuthService } from '../../core/auth.service';
+import { Subject, takeUntil } from 'rxjs';
 @Component({
   selector: 'login',
   standalone: true,
@@ -13,11 +15,33 @@ import {
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
+  authService = inject(AuthService);
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
   });
+
+  loading = signal<boolean>(false);
+  $destroy = new Subject<void>();
+
   ngOnInit(): void {
     console.log('init');
+  }
+
+  handleLogin(): void {
+    const { email, password } = this.loginForm.value;
+
+    if (email?.length && password?.length) {
+      this.loading.set(true);
+      this.authService
+        .login({ email, password })
+        .pipe(takeUntil(this.$destroy))
+        .subscribe({
+          next: (res) => console.log(res),
+          error: (err) => console.log('err:', err),
+          complete: () => this.loading.set(false),
+        });
+    }
   }
 }
