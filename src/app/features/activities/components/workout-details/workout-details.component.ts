8d470c24a0f92fddc5948aa8c37d31a65d6a09a1
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2025-01-04 11:00:19 +0000

    added host listener to open and close workout details on click

--- src/app/features/activities/components/workout-details/workout-details.component.ts
+++ src/app/features/activities/components/workout-details/workout-details.component.ts
@@ -1,13 +1,22 @@
-import { Component, inject, input } from '@angular/core';
-import { SelectedWorkoutsService } from '../../services/selected-workouts.service';
+import { Component, HostListener, inject, input } from '@angular/core';
 import { Workout } from '../../types/Workout';
+import { DatePipe } from '@angular/common';
+import { SelectedWorkoutsService } from '../../services/selected-workouts.service';
 
 @Component({
   selector: 'workout-details',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './workout-details.component.html',
   styleUrl: './workout-details.component.scss',
 })
 export class WorkoutDetailsComponent {
+  selectedWorkoutsService = inject(SelectedWorkoutsService);
+  @HostListener('window:click', ['$event'])
+  handleClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (target.id !== 'workout-details' && target.id !== 'active-day') {
+      this.selectedWorkoutsService.selectedWorkouts = undefined;
+    }
+  }
   workouts = input.required<Workout[]>();
 }
