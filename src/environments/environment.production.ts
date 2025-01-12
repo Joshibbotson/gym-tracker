94bc134583aae61838f117a5fb2fa85c5dae9758
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2025-01-12 13:18:52 +0000

    updated backend domain

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gym-tracker-backend-production-3816.up.railway.app',
+  apiUrl: 'https://gym-tracker-backend.joshibbotson.com',
 };
