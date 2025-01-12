23e2e6d07a71f1308194e4329b8d7bc43b82e5bc
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2025-01-12 11:35:24 +0000

    production check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'gym-tracker';
+
+  constructor() {
+    console.log(environment.production);
+  }
 }
